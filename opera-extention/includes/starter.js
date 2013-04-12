3d9874a4e9c502976cbff06434ff875e4256c6bf
uhyo <uhyohyo@uhyohyohyo.sakura.ne.jp>
2013-04-13 01:19:12 +0900

    b

--- opera-extention/includes/starter.js
+++ opera-extention/includes/starter.js
@@ -3,6 +3,12 @@
 opera.extension.onmessage=function(e){
 	if(e.data==="activate"){
 		//エクステンション起動
-		var i=new Interface(new EventClient(e.source));
+		var t=opera.extension.tabs.getSelected();
+		if(t && t.browserWindow==window){
+			//自分が起動するぞ!
+			var i=new Interface(new EventClient(e.source));
+			//返事する
+			e.source.postMessage("ok");
+		}
 	}
 };
