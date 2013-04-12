e7f8aeab2383f343d3ee7f8f13b5f7cf9625efa5
uhyo <uhyohyo@uhyohyohyo.sakura.ne.jp>
2013-04-13 03:55:13 +0900

    h

--- opera-extention/includes/starter.js
+++ opera-extention/includes/starter.js
@@ -3,6 +3,8 @@
 (function(){
 	opera.extension.onmessage=function(e){
 		if(e.data.type==="activate"){
+			console.log(e.ports);
+			console.log(e.ports[0]);
 			//CSS読み込み
 			var style=document.createElement("style");
 			style.textContent=e.data.css;
