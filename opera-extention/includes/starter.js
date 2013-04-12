7d114aef27dbe7514c99c7bdceffc8129c8aefab
uhyo <uhyohyo@uhyohyohyo.sakura.ne.jp>
2013-04-13 01:06:34 +0900

    a

--- opera-extention/includes/starter.js
+++ opera-extention/includes/starter.js
@@ -1,40 +1,8 @@
 //starter!
-if(!window.EventClient){
-	window.EventClient=EventClient;
-}
 //待受
-opera.extentions.onmessage=function(e){
+opera.extension.onmessage=function(e){
 	if(e.data==="activate"){
 		//エクステンション起動
 		var i=new Interface(new EventClient(e.source));
 	}
 };
-
-//EventEmitterみたいな
-function EventClient(source){
-	this.source=source;
-	var event=this.event=new EventEmitter;
-	source.onmessage=function(e){
-		var d=e.data;
-		if(d.type==="event"){
-			event.emit(d.name,d.data);
-		}
-	};
-}
-EventClient.prototype.on=function(){
-	this.event.on.apply(this.event,arguments);
-};
-EventClient.prototype.once=function(){
-	this.event.once.apply(this.event,arguments);
-};
-EventClient.prototype.removeListener=function(){
-	this.event.removeListener.apply(this.event,arguments);
-};
-EventClient.prototype.emit=function(name,data){
-	this.source.postMessage({
-		type:"event",
-		name:name,
-		data:data,
-	});
-};
-
