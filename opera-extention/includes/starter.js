f491382da370d69ac2852ccf4f4cc0dcfc14a287
uhyo <uhyohyo@uhyohyohyo.sakura.ne.jp>
2013-04-13 02:55:12 +0900

    f

--- opera-extention/includes/starter.js
+++ opera-extention/includes/starter.js
@@ -2,7 +2,11 @@
 //待受
 (function(){
 	opera.extension.onmessage=function(e){
-		if(e.data==="activate"){
+		if(e.data.type==="activate"){
+			//CSS読み込み
+			var style=document.createElement("style");
+			style.textContent=e.data.css;
+			document.head.appendChild(style);
 			//エクステンション起動
 			var i=new Interface(new EventClient(e.source));
 			//返事する
