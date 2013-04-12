3dcfbd3d59deac315675cea840092e015e98f374
uhyo <uhyohyo@uhyohyohyo.sakura.ne.jp>
2013-04-13 02:20:01 +0900

    e

--- opera-extention/includes/starter.js
+++ opera-extention/includes/starter.js
@@ -1,10 +1,25 @@
 //starter!
 //待受
-opera.extension.onmessage=function(e){
-	if(e.data==="activate"){
-		//エクステンション起動
-		var i=new Interface(new EventClient(e.source));
-		//返事する
-		e.source.postMessage("ok");
+(function(){
+	var id=getUniqueid();
+	opera.extension.onmessage=function(e){
+		if(e.data.type==="activate" && e.data.id===id){
+			//エクステンション起動
+			var i=new Interface(new EventClient(e.source));
+			//返事する
+			e.source.postMessage("ok");
+		}
+	};
+	opera.extension.postMessage({
+		type:"myid",
+		id:id,
+	});
+	function getUniqueid(){
+		var result="";
+		for(var i=0;i<50;i++){
+			//a-z
+			result+=String.fromCharCode(97+Math.floor(Math.random()*26));
+		}
+		return result;
 	}
-};
+})();
